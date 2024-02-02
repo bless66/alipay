763b60682fee2e9830b0ad90d2e765882a3b45a7
351539365@qq.com <351539365@qq.com>
2024-02-02 15:20:15 +0800

    测试提交

---
+++ test-cloud-develop/src/org/alipay/code/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Ant Group
+ * Copyright (c) 2004-2024 All Rights Reserved.
+ */
+package org.alipay.code;
+
+/**
+ * @author EMAILPREFIX
+ * @version Test.java, v 0.1 2024年02月02日 下午3:09 EMAILPREFIX
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
+}
